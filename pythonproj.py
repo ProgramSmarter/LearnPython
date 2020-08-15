97e5185faa097d3e42f9d336c1169aee373859a4
ProgramSmart <26singhishan@gmail.com>
2020-08-15 09:05:57 -0700

    heh

--- pythonproj.py
+++ pythonproj.py
@@ -3,32 +3,28 @@ def all():
     list2 = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
     list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     def funct():
-            y = input("Type in your move. (1-9)")
-            x = y
-            y = int(y)
-            y -= 1
+            x = input("Type in your move. (1-9)")
             x = int(x)
-            if int(y) not in list:
+            x -= 1
+            if x not in list:
                 print("That didn't work! Try again!")
                 funct()
             else:
-                list.remove(int(y))
-                x -= 1
-                list2[int(x)] = "X"
+                list.remove(x)
+                list2[x] = "X"
             print(list2[0:3])
             print(list2[3:6])
             print(list2[6:9])
-            compchoicerandom = random.choice(list)
-            compchoicerandom = int(compchoicerandom)
+            compchoicerandom = int(random.choice(list))
             list2[compchoicerandom] = 'O'
             list.remove(int(compchoicerandom))
             print("I've made my move!")
-            if list2[0] and list2[1] and list2[2] == "O" or list2[3] and list2[4] and list2[5] == "O" or list2[6] and list2[7] and list2[8] == "O" or list2[0] and list2[3] and list2[6] == "O" or list2[1] and list2[4] and list2[7] == "O" or list2[2] and list2[5] and list2[8] == "O" or list2[0] and list2[4] and list2[8] == "O" or list2[2] and list2[4] and list2[6] == "O":
+            if (list2[0] and list2[1] and list2[2] == "O") or (list2[3] and list2[4] and list2[5] == "O") or (list2[6] and list2[7] and list2[8] == "O") or (list2[0] and list2[3] and list2[6] == "O") or (list2[1] and list2[4] and list2[7] == "O") or (list2[2] and list2[5] and list2[8] == "O") or (list2[0] and list2[4] and list2[8] == "O") or (list2[2] and list2[4] and list2[6] == "O"):
                 print("You have lost! :(")
                 all()
             else:
                 pass
-            if list2[0] and list2[1] and list2[2] == "X" or list2[3] and list2[4] and list2[5] == "X" or list2[6] and list2[7] and list2[8] == "X" or list2[0] and list2[3] and list2[6] == "X" or list2[1] and list2[4] and list2[7] == "X" or list2[2] and list2[5] and list2[8] == "X" or list2[0] and list2[4] and list2[8] == "X" or list2[2] and list2[4] and list2[6] == "X":
+            if (list2[0] and list2[1] and list2[2] == "X") or (list2[3] and list2[4] and list2[5] == "X") or (list2[6] and list2[7] and list2[8] == "X") or (list2[0] and list2[3] and list2[6] == "X") or (list2[1] and list2[4] and list2[7] == "X") or (list2[2] and list2[5] and list2[8] == "X") or (list2[0] and list2[4] and list2[8] == "X") or (list2[2] and list2[4] and list2[6] == "X"):
                 print("You have won! :)")
                 all()
             else:    
