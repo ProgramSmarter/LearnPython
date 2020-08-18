54d6b4582d337af31049a49e70e117d010d36326
ProgramSmart <26singhishan@gmail.com>
2020-08-18 09:31:49 -0700

    WASEDFRTGHUIOJK

--- numpy.py
+++ numpy.py
@@ -1,17 +1,2 @@
-x = int(input("Input number!"))
-x += 1
-d = 0
-for different in range(1, x):
-    print(different)
-    for diff in range(different):
-        diff += 1
-        print(diff, end = '')
-x -= 2
-for different in range(x, 1, -1):
-    print()   
-    for diff in range(different):
-        diff += 1
-        print(diff, end = "")
-        diff -= 1
-print()
-print(1)
+def funct():
+    x = str(input("Give me a full name."))
