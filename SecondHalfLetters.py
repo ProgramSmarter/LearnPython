4793d84d15df685ffe75b8161892e9a1f7b0ebea
ProgramSmart <69406870+ProgramSmarter@users.noreply.github.com>
2020-09-06 14:10:00 -0700

    heho

--- SecondHalfLetters.py
+++ SecondHalfLetters.py
@@ -1,21 +1,4 @@
-x = int(input("Type in a number. "))
-y = int(input("Type in another number. "))
-z = int(input("Type in a final number. "))
-if x < y < z:
-    glob = ("The median of your three numbers is {}.")
-    print(z.format(y))
-if z < y < x:
-    glob = ("The median of your three numbers is {}.")
-    print(glob.format(y))
-if y < x < z:
-    glob = ("The median of your three numbers is {}.")
-    print(glob.format(x))
-if z < x < y:
-    glob = ("The median of your three numbers is {}.")
-    print(glob.format(x))
-if y < z < x:
-    glob = ("The median of your three numbers is {}.")
-    print(glob.format(z))
-if z < x < y:
-    glob = ("The median of your three numbers is {}.")
-    print(glob.format(z))
+import numpy
+arr = numpy.array([1, 2, 3, 4, 5])
+
+print(arr)
