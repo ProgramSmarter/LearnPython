34e14a6c0fc96556cb5c0dd8d36b1f9568e48170
ProgramSmart <69406870+ProgramSmarter@users.noreply.github.com>
2020-09-13 12:37:14 -0700

    Update SecondHalfLetters.py

--- SecondHalfLetters.py
+++ SecondHalfLetters.py
@@ -1,6 +1,5 @@
-import kivy
-from kivy.app import App
-from kivy.uix.label import Label
-class MyFirstKivyApp(App):
-    def build(self):
-        return Label(text ="Hello World !")
+import numpy as np
+
+arr = np.array((1, 2, 3, 4, 5))
+
+print(arr)
